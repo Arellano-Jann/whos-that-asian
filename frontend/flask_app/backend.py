f7e001b17c69caac4f61f7e65200122c665e8d92
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-08-24 14:11:49 -0700

    added new ml and updated to have predict route

--- frontend/flask_app/backend.py
+++ frontend/flask_app/backend.py
@@ -1,22 +1,26 @@
 from flask import Flask, send_from_directory
-import random
+from ml.faceRecognition import main as FaceRecognition
 
 app = Flask(__name__)
 
-# Path for our main Svelte page
-@app.route("/")
-def base():
-    return send_from_directory('client/public', 'index.html')
+# # Path for our main Svelte page
+# @app.route("/")
+# def base():
+#     return send_from_directory('../svelte', 'index.html')
 
-# Path for all the static files (compiled JS/CSS, etc.)
-@app.route("/<path:path>")
-def home(path):
-    return send_from_directory('client/public', path)
+# # Path for all the static files (compiled JS/CSS, etc.)
+# @app.route("/<path:path>")
+# def home(path):
+#     return send_from_directory('../svelte', path)
 
 
-@app.route("/rand")
-def hello():
-    return str(random.randint(0, 100))
+# @app.route("/rand")
+# def hello():
+#     return str(random.randint(0, 100))
+
+@app.route("/predict")
+def predict(image):
+    return FaceRecognition(image)
 
 
 if __name__ == "__main__":
