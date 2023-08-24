33a7927d468f0ffe7f15bd73803310d9386a4b9a
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-08-24 13:54:00 -0700

    some stuf

--- frontend/svelte/src/App.svelte
+++ frontend/svelte/src/App.svelte
@@ -1,47 +1,48 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+	let  avatar, fileinput;
+	
+	const onFileSelected =(e)=>{
+    let image = e.target.files[0];
+            let reader = new FileReader();
+            reader.readAsDataURL(image);
+            reader.onload = e => {
+                 avatar = e.target.result
+            };
+    }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<div id="app">
+	<h1>Upload Image</h1>  
+        {#if avatar}
+        <img class="avatar" src="{avatar}" alt="d" />
+        <p>Result: Something</p>
+        {:else}
+        <img class="avatar" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" alt="" /> 
+        <p>Result: None</p>
+        {/if}
+				<img class="upload" src="https://static.thenounproject.com/png/625182-200.png" alt="" on:click={()=>{fileinput.click();}} />
+        <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
+</div>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	#app{
+	display:flex;
+		align-items:center;
+		justify-content:center;
+		flex-flow:column;
+}
+ 
+	.upload{
+		display:flex;
+	height:50px;
+		width:50px;
+		cursor:pointer;
+	}
+	.avatar{
+		display:flex;
+		height:200px;
+		width:200px;
+	}
 </style>
+
+ 
---
+++ todo.md
@@ -0,0 +1,10 @@
+attach svelte frontend to flask backend
+1. frontend/flask_app/backend.py - modify the pasted backend with "using fetch and ML and Flask backend"
+2. frontend/svelte/src/App.svelte - modify the pasted to use forms instead
+
+part 2
+upgrade and connect the flask backend to sveltekit using fetch
+1. 
+
+part 3
+deploy sveltekit to cloudflare
